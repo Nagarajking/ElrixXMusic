b3828afe8b65b12f2adbfda5f50b8ad3698e565b
Nagarajking <87459202+Nagarajking@users.noreply.github.com>
2022-12-31 12:28:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ if os.path.exists("local.env"):
 
 load_dotenv()
 que = {}
-SESSION_NAME = getenv("SESSION_NAME", "BQDAvaxt8-WR1nh6abaKTltCO4pXbxtwRysf2WPHWkS6UBaNcysJaXwP08CWssjnfUl8i9Ku8hcWWiPFdX6DYMd5ba9obDv9lsMlMyfwMegZ6V90ga8Zb3s-m6J6KPky41dt7T0mWfiBTi0Shrsr4MJn1FpAHDvNSUXdYk8pw3zvZhKH_MyMjt3NzfI3VausA4SasXuiUMJodlHLcqJBiBEYnNfmY48IwKONkd8xa2896VIXoUV_YTReyQ0TqFtJb00sFb4FP8TxmoOGnY61BePX3Rm6YhRZZEKevCiCNghDIAWrFo5Df9SLzeM6Qb79SuUbpUu3n_ZraVUqiNCMK4RGAAAAAFR7FIkA")
+SESSION_NAME = getenv("SESSION_NAME", "BQBDs6xBrEi1fBwJXQFuN6DYQB8vVza7lPzjLiQ70_cwjg3MjU4d1mPb8DiHEjOs4Zbpy3Ty5RWHOL6PMXekcBjLeB1MxM_ib6f9vPosGllKVhubnsNT67FbbcgwNuQSlyZgim1PY5_nm-2qTxHATCW4CgWqzt_NErplRmetDa8vJ_d43ycVobOiDZx08iDVgoxljjfAddz9FwECZUKOSqbV3RAhk8JFj4cJGoONytuxyfBP5E10AEhuvyZYiGsCqhGQTWkylgXCdU_JNGTNU-tEFGpbNMgIoxhsWWgdRO_HsppVj8u0-HSqZHJHjX7KPJfe8ofbs3ru5VbWzxUuk84UVHsUiQA")
 BOT_TOKEN = getenv("2084060024:AAG2qH4BL9PXPKFnvmuIxwDjcKFJG-jLXEQ")
 BOT_NAME = getenv("BOT_NAME", "ElrixXMusic")
 BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/ccdb7dd3392bc90248472.jpg")
